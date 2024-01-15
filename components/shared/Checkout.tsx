dfcf4e219bcf6a26e299fa4f45ff9f35059c867b
tugume frank <frankholmez@gmail.com>
2024-01-15 14:52:08 +0300

    flutter implementation

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,22 +1,33 @@
-import React, { useEffect } from 'react'
-import { loadStripe } from '@stripe/stripe-js';
+import React, { useEffect } from "react";
 
-import { IEvent } from '@/lib/database/models/event.model';
-import { Button } from '../ui/button';
-import { checkoutOrder } from '@/lib/actions/order.actions';
-
-loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
-
-const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
+import { IEvent } from "@/lib/database/models/event.model";
+import { Button } from "../ui/button";
+import { checkoutOrder } from "@/lib/actions/order.actions";
+import { useFlutterwave, closePaymentModal } from "flutterwave-react-v3";
+const flutter_key = process.env.REACT_APP_FLUTTER_PUBLIC_KEY!;
+console.log(flutter_key);
+const Checkout = ({
+  event,
+  userId,
+  email,
+  userImage,
+}: {
+  event: IEvent;
+  userId: string;
+  email: string;
+  userImage: string;
+}) => {
   useEffect(() => {
     // Check to see if this is a redirect back from Checkout
     const query = new URLSearchParams(window.location.search);
-    if (query.get('success')) {
-      console.log('Order placed! You will receive an email confirmation.');
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
     }
 
-    if (query.get('canceled')) {
-      console.log('Order canceled -- continue to shop around and checkout when you’re ready.');
+    if (query.get("canceled")) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you’re ready."
+      );
     }
   }, []);
 
@@ -26,19 +37,81 @@ const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
       eventId: event._id,
       price: event.price,
       isFree: event.isFree,
-      buyerId: userId
-    }
+      buyerId: userId,
+    };
 
     await checkoutOrder(order);
-  }
+  };
+  type Customer = {
+    email: string;
+    phone_number: string;
+    name: string;
+  };
 
+  type Customizations = {
+    title: string;
+    description: string;
+    logo: string;
+  };
+
+  type FlutterwaveConfig = {
+    public_key: string;
+    tx_ref: string;
+    amount: number;
+    currency: string;
+    payment_options: string;
+    customer: Customer;
+    customizations: Customizations;
+  };
+  // if (!flutter_key) {
+  //   return (
+  //     <div>
+  //       <p>Error: Flutterwave public key is not defined.</p>
+  //       {/* Optionally, provide instructions for setting the key */}
+  //     </div>
+  //   );
+  // }
+  console.log(flutter_key);
+  const config: FlutterwaveConfig = {
+    public_key: flutter_key,
+    tx_ref: Date.now().toString(),
+    amount: parseFloat(event.price),
+    currency: "UGX",
+    payment_options: "card,mobilemoneyuganda",
+    customer: {
+      email: email,
+      phone_number: "070********",
+      name: "john doe",
+    },
+    customizations: {
+      title: event.title,
+      description: event.description,
+      logo: userImage,
+    },
+  };
+
+  const handleFlutterPayment = useFlutterwave(config);
   return (
-    <form action={onCheckout} method="post">
-      <Button type="submit" role="link" size="lg" className="button sm:w-fit">
-        {event.isFree ? 'Get Ticket' : 'Buy Ticket'}
-      </Button>
-    </form>
-  )
-}
+    // <form action={onCheckout} method="post">
+    <Button
+      onClick={() => {
+        handleFlutterPayment({
+          callback: (response) => {
+            console.log(response);
+            closePaymentModal(); // this will close the modal programmatically
+          },
+          onClose: () => {},
+        });
+      }}
+      type="submit"
+      role="link"
+      size="lg"
+      className="button sm:w-fit"
+    >
+      {event.isFree ? "Get Ticket" : "Buy Ticket"}
+    </Button>
+    // </form>
+  );
+};
 
-export default Checkout
+export default Checkout;
--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -1,38 +1,46 @@
-"use client"
+"use client";
 
-import { IEvent } from '@/lib/database/models/event.model'
-import { SignedIn, SignedOut, useUser } from '@clerk/nextjs'
-import Link from 'next/link'
-import React from 'react'
-import { Button } from '../ui/button'
-import Checkout from './Checkout'
+import { IEvent } from "@/lib/database/models/event.model";
+import { SignedIn, SignedOut, useUser } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import Checkout from "./Checkout";
 
 const CheckoutButton = ({ event }: { event: IEvent }) => {
   const { user } = useUser();
+  console.log(user);
   const userId = user?.publicMetadata.userId as string;
+  const email = user?.primaryEmailAddress?.emailAddress as string;
+  const userImage = user?.imageUrl as string;
   const hasEventFinished = new Date(event.endDateTime) < new Date();
 
   return (
     <div className="flex items-center gap-3">
       {hasEventFinished ? (
-        <p className="p-2 text-red-400">Sorry, tickets are no longer available.</p>
-      ): (
+        <p className="p-2 text-red-400">
+          Sorry, tickets are no longer available.
+        </p>
+      ) : (
         <>
           <SignedOut>
             <Button asChild className="button rounded-full" size="lg">
-              <Link href="/sign-in">
-                Get Tickets
-              </Link>
+              <Link href="/sign-in">Get Tickets</Link>
             </Button>
           </SignedOut>
 
           <SignedIn>
-            <Checkout event={event} userId={userId} />
+            <Checkout
+              event={event}
+              userId={userId}
+              email={email}
+              userImage={userImage}
+            />
           </SignedIn>
         </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default CheckoutButton
+export default CheckoutButton;
