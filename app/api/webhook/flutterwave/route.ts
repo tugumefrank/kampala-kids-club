ffbf3de032d87603786cfc026e570febef6b8178
tugume frank <frankholmez@gmail.com>
2024-01-17 02:56:17 +0300

    more on webhooks

--- app/api/webhook/flutterwave/route.ts
+++ app/api/webhook/flutterwave/route.ts
@@ -48,6 +48,7 @@ import { createOrder } from "@/lib/actions/order.actions";
 export async function POST(request: Request, response: Response) {
   const body = await request.text();
 
+  const flutterwaveSecretKey = process.env.FLW_SECRET_KEY;
   const secretHash = process.env.FLW_SECRET_HASH;
   const signature = request.headers.get("verif-hash") as string;
   if (!signature || signature !== secretHash) {
@@ -67,16 +68,16 @@ export async function POST(request: Request, response: Response) {
 
   // Check if the event is successful
   if (eventStatus === "successful") {
-    const { id, amount_total, metadata } = eventData.meta;
+    const { id, amount, txRef } = eventData;
 
     // Make a GET request to Flutterwave API
-    const txRef = metadata?.tx_ref || ""; // Replace with the actual key you expect in metadata
-    const flutterwaveApiResponse = await fetch(
+    // const txRef = metadata?.txRef || ""; // Replace with the actual key you expect in metadata
+    const transactionData = await fetch(
       `https://api.flutterwave.com/v3/transactions/verify_by_reference?tx_ref=${txRef}`,
       {
         method: "GET",
         headers: {
-          Authorization: `Bearer YOUR_FLUTTERWAVE_API_KEY`, // Replace with your Flutterwave API key
+          Authorization: `Bearer ${flutterwaveSecretKey}`, // Replace with your Flutterwave API key
           "Content-Type": "application/json",
           // Add any additional headers if needed
         },
@@ -84,30 +85,28 @@ export async function POST(request: Request, response: Response) {
       }
     );
 
-    if (flutterwaveApiResponse.ok) {
-      const flutterwaveApiResponseData = await flutterwaveApiResponse.json();
+    if (transactionData.ok) {
+      const transactionDetails = await transactionData.json();
+      const { eventId } = transactionDetails.data.meta;
+      // Your existing code to create an order
+      const order = {
+        stripeId: id,
+        eventId: eventId,
+        buyerId: transactionDetails.data?.id || "",
+        totalAmount: amount ? (amount / 100).toString() : "0",
+        createdAt: new Date(),
+      };
 
-      // Process the Flutterwave API response data as needed
-      console.log(flutterwaveApiResponseData);
+      const newOrder = await createOrder(order);
+      return NextResponse.json({ message: "OK", order: newOrder });
+      console.log(transactionDetails);
     } else {
       console.error(
         "Failed to fetch Flutterwave API:",
-        flutterwaveApiResponse.status,
-        flutterwaveApiResponse.statusText
+        transactionData.status,
+        transactionData.statusText
       );
     }
-
-    // Your existing code to create an order
-    const order = {
-      stripeId: id,
-      eventId: metadata?.eventId || "",
-      buyerId: metadata?.buyerId || "",
-      totalAmount: amount_total ? (amount_total / 100).toString() : "0",
-      createdAt: new Date(),
-    };
-
-    const newOrder = await createOrder(order);
-    return NextResponse.json({ message: "OK", order: newOrder });
   }
 
   return new Response("", { status: 200 });
