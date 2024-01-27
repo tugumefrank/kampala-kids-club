3b1294b675d60af1d79765ce83e9557e05786011
tugume frank <frankholmez@gmail.com>
2024-01-27 16:50:41 +0300

    from laptop

---
+++ app/api/twilio/route.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import twilio from "twilio";
+
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  const accountSid = <string>process.env.TWILIO_ACCOUNT_SID;
+  const token = <string>process.env.TWILIO_AUTH_TOKEN;
+  const client = twilio(accountSid, token);
+  const { phone, message } = req.body;
+  console.log(phone, message);
+  client.messages
+    .create({
+      body: "FRANK HOW ARE YOU",
+      from: "+14155238886",
+      to: "+256709738858",
+    })
+    .then((message) =>
+      res.json({
+        success: true,
+        message,
+      })
+    )
+    .catch((error) => {
+      console.log(error);
+      res.json({
+        success: false,
+      });
+    });
+}
