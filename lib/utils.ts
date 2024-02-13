748ffc632e7d18218332bd2e51678109610a140b
tugume frank <frankholmez@gmail.com>
2024-02-13 21:59:25 +0300

    fixed kaban error

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,34 @@
-import { type ClassValue, clsx } from "clsx";
-
-import { twMerge } from "tailwind-merge";
 import qs from "query-string";
 
 import { UrlQueryParams, RemoveUrlQueryParams } from "@/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { Active, DataRef, Over } from "@dnd-kit/core";
+import { ColumnDragData } from "@/components/kanban/board-column";
+import { TaskDragData } from "@/components/kanban/task-card";
+
+type DraggableData = ColumnDragData | TaskDragData;
+
+export function hasDraggableData<T extends Active | Over>(
+  entry: T | null | undefined
+): entry is T & {
+  data: DataRef<DraggableData>;
+} {
+  if (!entry) {
+    return false;
+  }
+
+  const data = entry.data.current;
+
+  if (data?.type === "Column" || data?.type === "Task") {
+    return true;
+  }
+
+  return false;
 }
 
 export const formatDateTime = (dateString: Date) => {
