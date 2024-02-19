6788aec461ace5d6cdce4eb123275b26a7858125
tugume frank <frankholmez@gmail.com>
2024-02-19 22:27:58 +0300

    mongoose conn

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,44 +1,22 @@
-// import mongoose from 'mongoose';
-
-// const MONGODB_URI = process.env.MONGODB_URI;
-
-// let cached = (global as any).mongoose || { conn: null, promise: null };
-
-// export const connectToDatabase = async () => {
-//   if (cached.conn) return cached.conn;
-
-//   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
-
-//   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-//     dbName: 'evently',
-//     bufferCommands: false,
-//   })
-
-//   cached.conn = await cached.promise;
-
-//   return cached.conn;
-// }
-
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cachedConnection: any;
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-  if (cachedConnection) return cachedConnection;
+  if (cached.conn) return cached.conn;
 
   if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
-  try {
-    cachedConnection = await mongoose.connect(MONGODB_URI, {
-      bufferCommands: true, // Adjust if needed
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "evently",
+      bufferCommands: false,
     });
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    throw error;
-  }
 
-  return cachedConnection;
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 };
