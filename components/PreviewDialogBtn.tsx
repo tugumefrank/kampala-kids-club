c3cd4deecfc4ebbcf6b3ee95c3d506770bf87cc8
tugume frank <frankholmez@gmail.com>
2024-02-21 02:52:47 +0300

    dark mode in form preview

--- components/PreviewDialogBtn.tsx
+++ components/PreviewDialogBtn.tsx
@@ -17,15 +17,17 @@ function PreviewDialogBtn() {
           Preview
         </Button>
       </DialogTrigger>
-      <DialogContent className="w-screen h-screen max-h-screen max-w-full bg-[#E2E8F0]  flex flex-col flex-grow p-0 gap-0">
+      <DialogContent className="w-screen h-screen max-h-screen max-w-full bg-[#E2E8F0]  dark:bg-slate-950 flex flex-col flex-grow p-0 gap-0">
         <div className="px-4 py-2 border-b">
-          <p className="text-lg font-bold text-black ">Form preview</p>
-          <p className="text-sm text-black">
+          <p className="text-lg font-bold text-black dark:text-white">
+            Form preview
+          </p>
+          <p className="text-sm text-black dark:text-white">
             This is how your form will look like to your users.
           </p>
         </div>
         <div className="bg-accent flex flex-col flex-grow items-center justify-center p-4 bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)] overflow-y-auto">
-          <div className="max-w-[700px] flex flex-col gap-4 flex-grow bg-[#E2E8F0] h-full w-full rounded-2xl p-8 overflow-y-auto">
+          <div className="max-w-[700px] flex flex-col gap-4 flex-grow bg-[#E2E8F0] dark:bg-slate-950 h-full w-full rounded-2xl p-8 overflow-y-auto">
             {elements.map((element) => {
               const FormComponent = FormElements[element.type].formComponent;
               return (
