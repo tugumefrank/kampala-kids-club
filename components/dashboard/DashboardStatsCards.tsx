2a7dfbb21e7a686d45d870952c37006b638e304d
tugume frank <frankholmez@gmail.com>
2024-02-24 12:27:40 +0300

    ui updates dashboard overview cards

--- components/dashboard/DashboardStatsCards.tsx
+++ components/dashboard/DashboardStatsCards.tsx
@@ -13,8 +13,8 @@ const DashboardStatsCards = async () => {
   return (
     <>
       <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <Card className="shadow-md border-t-4 border-blue-600">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 ">
             <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -24,7 +24,7 @@ const DashboardStatsCards = async () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              className="h-4 w-4 text-primary"
+              className="h-4 w-4 text-blue-600"
             >
               <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
             </svg>
@@ -36,7 +36,7 @@ const DashboardStatsCards = async () => {
             </p>
           </CardContent>
         </Card>
-        <Card>
+        <Card className="shadow-md border-t-4 border-yellow-600">
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">
               SignUp Revenue
@@ -49,7 +49,7 @@ const DashboardStatsCards = async () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              className="h-4 w-4 text-primary"
+              className="h-4 w-4 text-yellow-600"
             >
               <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
               <circle cx="9" cy="7" r="4" />
@@ -63,7 +63,7 @@ const DashboardStatsCards = async () => {
             </p>
           </CardContent>
         </Card>
-        <Card>
+        <Card className="shadow-md border-t-4 border-green-600">
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Votes Revenue</CardTitle>
             <svg
@@ -74,7 +74,7 @@ const DashboardStatsCards = async () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              className="h-4 w-4 text-primary"
+              className="h-4 w-4 text-green-600"
             >
               <rect width="20" height="14" x="2" y="5" rx="2" />
               <path d="M2 10h20" />
@@ -87,7 +87,7 @@ const DashboardStatsCards = async () => {
             </p>
           </CardContent>
         </Card>
-        <Card>
+        <Card className="shadow-md border-t-4 border-primary">
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">
               Total Withdraws
